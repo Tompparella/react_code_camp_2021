4f76473ff241a588552c4949d1449c224ea96ba3
Tommi <tommi.kunnari@hotmail.com>
2021-01-08 16:51:01 +0200

    Added a header

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -1,9 +1,13 @@
 import "./main.css";
+import Header from "../header";
 
 function Main() {
     return (
       <div className="Main">
           <p>Welcome to react!</p>
+          <div className="header">
+            <Header/>
+          </div>
       </div>
     );
   }
