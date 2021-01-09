71124560469218ab629eec51d805096b75f780fb
Joonas Kukonlehto <joonas.kukonlehto@gmail.com>
2021-01-09 18:24:41 +0200

    Added Kauppa button.

---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+    render() {
+        return(
+            <div id="button">
+                <div><button>Kauppa</button></div>
+            </div>
+        )
+    }
+}
--- src/components/pages/testPage.js
+++ src/components/pages/testPage.js
@@ -1,8 +1,12 @@
+import Button from "../button";
 
 function Test() {
     return (
       <div className="Test">
           <p>This is a test page</p>
+        <div className="button">
+          <Button/>
+        </div>
       </div>
     );
   }
