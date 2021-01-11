02334e3b183109c4f9800eed9e98d57f0a411f65
Flankmain <aleksanteri.reijo@gmail.com>
2021-01-11 18:24:50 +0200

    clickbutton pikkumuutos

--- src/components/pages/ClickButton.js
+++ src/components/pages/ClickButton.js
@@ -5,16 +5,19 @@ export default function ClickButton( props ) {
 
     const [count, setCount] = useState(props.count ? props.count : 0);
 
-    function onClick()
+    function increment()
     {
-        console.log('nappia painettu');
-        setCount(count + 1);
+        let inc = 1;
+        //TODO: lisää vaihtuva inkrementti johonkin kansioon
+
+        console.log('Nappia painettu. Uusi arvo: ' + (count + inc));
+        setCount(count + inc);
     }
 
     return (
         <div>
             <p>This button has been clicked {count} times.</p>
-            <img src={image} onClick={onClick}/>
+            <img src={image} onClick={increment}/>
         </div>
     );
 }
