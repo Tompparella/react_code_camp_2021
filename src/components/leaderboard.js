7187e7acdd7efc3bd4362ba37eaaf9c30900b25b
Tommi <tommi.kunnari@hotmail.com>
2021-01-14 15:59:29 +0200

    Fixed the leaderboard

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -19,12 +19,15 @@ export default class Leaderboard extends Component {
             response.json()
             .then((data) => {
 
-                if (data == null) {
+                if (data[0] == null) {
                     console.log("No games found");
                     return;
                 }
-                let len = data.length;
-                for (let i=(len-1); i>=0; i--) {
+                data.sort((a,b) => {
+                    return b.score - a.score;
+                });
+                console.log(data);
+                for (let i=0; i<10; i++) {
     
                     let game = {
                         player: data[i].player,
